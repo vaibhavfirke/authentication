e0558c78efb574d6212fc6447d3c8111244b8e88
vaibhavfirke <firkevaibhav11@gmail.com>
2023-02-25 14:34:41 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 const express=require('express');
+const cors=require('cors')
 const { connection } = require('./config/db');
 const { UserRout } = require('./routes/user.routes');
 const app=express();
-
+app.use(cors)
 app.use(express.json());
 app.use("/api",UserRout)
 
